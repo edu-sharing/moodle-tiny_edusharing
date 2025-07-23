95f219a0c64b8607e3d2b035f40d571653e0ae95
ziegler <ziegler@edu-sharing.net>
2025-07-23 11:34:37 +0200

    fix: wrong version number

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component    = 'tiny_edusharing';
 $plugin->release      = '10.1.0';
-$plugin->version      = 2025071400;
+$plugin->version      = 2025072300;
 $plugin->requires     = 2022112809;
 $plugin->dependencies = ['mod_edusharing' => 2024072300];
 $plugin->maturity     = MATURITY_STABLE;
